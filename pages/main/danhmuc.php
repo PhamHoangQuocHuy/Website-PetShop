bd30dd733c0b4c8e4a4d3e632a52ca4d476d3ed6
Phạm Hoàng Quốc Huy <quochuy6422@gmail.com>
2023-12-15 17:19:28 +0700

    hien thi sp tren danh muc sp

--- pages/main/danhmuc.php
+++ pages/main/danhmuc.php
@@ -1,9 +1,13 @@
 <?php
-$sql_pro = "SELECT * FROM tbl_sanpham WHERE tbl_sanpham.id_danhmuc = '$_GET[id]' ORDER BY tbl_sanpham.id_sanpham DESC";
+$sql_pro = "SELECT * FROM tbl_sanpham WHERE tbl_sanpham.id_danhmuc = '$_GET[id]' ORDER BY id_sanpham DESC";
 $query_pro = mysqli_query($mysqli, $sql_pro);
+//get ten danh muc
+$sql_cate = "SELECT * FROM tbl_danhmuc WHERE tbl_danhmuc.id_danhmuc = '$_GET[id]' LIMIT 1";
+$query_cate = mysqli_query($mysqli, $sql_cate);
+$row_title = mysqli_fetch_array($query_cate);
 ?>
 
-<h3 class="title">DANH MỤC SẢN PHẨM: <?php $row_pro['tendanhmuc'] ?></h3>
+<h3 class="title">DANH MỤC SẢN PHẨM: <?php $row_title['tendanhmuc'] ?></h3>
 <ul class="product-list">
     <?php
     while ($row_pro = mysqli_fetch_array($query_pro)) {
